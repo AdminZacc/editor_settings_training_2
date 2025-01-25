45ba01d25db18bae6804303f849bad363eb73e88
AdminZacc <zacelliott17@gmail.com>
2025-01-24 20:31:52 -0500

    chore: basic project setup

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends(
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+), {
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+}, {
+    files: ["**/*"],
+    ignores: ["node_modules/**", "dist/**", "**/*.min.js"],
+}];
